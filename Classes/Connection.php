418a074b1079bd5c6e4bab8838551154b19cb910
Ilias Elbadaoui <iliassovich@outlook.com>
2022-06-14 16:59:39 +0100

    reading post

--- Classes/Connection.php
+++ Classes/Connection.php
@@ -6,7 +6,7 @@ class Connection
 
     public function __construct()
     {
-        $this->connection = new PDO("mysql:host=localhost;dbname=qor3a_dev","root","root");
+        $this->connection = new PDO("mysql:host=localhost;dbname=qor3a_dev","square","aitfaska98");
     }
     /**
      * @return PDO
