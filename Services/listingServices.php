04fae1191f6756664f436bab7e30b70861b19d8a
Ilias Elbadaoui <iliassovich@outlook.com>
2022-06-14 17:00:49 +0100

    reading post

--- Services/listingServices.php
+++ Services/listingServices.php
@@ -51,7 +51,7 @@ class listingServices
             $reg['pic3'] = "data:".$reg['pic3type'].";base64,".base64_encode(stripslashes($reg['pic3']));
             $reg['pic4'] = "data:".$reg['pic4type'].";base64,".base64_encode(stripslashes($reg['pic4']));
             array_push($newData, array('title'=>$reg['name'],'desc'=>$reg['description'], 'price'=>$reg['targetPrice'], 'collected'=>$reg['collectedFunds'], 'expires'=>$reg['dateEnd'],
-                $reg['pic1'],$reg['pic2'],$reg['pic3'],$reg['pic4']));
+                'pic1'=>$reg['pic1'], 'pic2'=>$reg['pic2'], 'pic3'=>$reg['pic3'], 'pic3'=>$reg['pic4']));
         }
         return $newData;
     }
