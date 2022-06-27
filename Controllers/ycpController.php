bd745a69bee1180c1ab0dd0c0c445f550abde947
Ilias Elbadaoui <iliassovich@outlook.com>
2022-06-27 14:58:09 +0100

    reading user's numbers

--- Controllers/ycpController.php
+++ Controllers/ycpController.php
@@ -1,7 +1,6 @@
 <?php
 
-include_once "../__root__.php";
-include_once root.'/vendor/youcanpay/payment-sdk/src/YouCanPay.php';
+use YouCan\Pay\YouCanPay;
 
 $youCanPay = YouCanPay::instance()->useKeys('pri_572164be-be2c-42d7-928a-66fc6680', 'pub_f9a7b5c1-2fc9-43b3-be3e-ee96b140');
 
