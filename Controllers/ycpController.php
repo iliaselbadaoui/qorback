98888e227048409f1cda10363e58852747eae26d
Ilias Elbadaoui <iliassovich@outlook.com>
2022-06-27 14:00:15 +0100

    reading user's numbers

--- Controllers/ycpController.php
+++ Controllers/ycpController.php
@@ -2,7 +2,7 @@
 
 use YouCan\Pay\YouCanPay;
 
-$youCanPay = YouCanPay::instance()->useKeys('pri_key', 'pub_key');
+$youCanPay = YouCanPay::instance()->useKeys('pri_572164be-be2c-42d7-928a-66fc6680', 'pub_f9a7b5c1-2fc9-43b3-be3e-ee96b140');
 
 // generate a token for a new payment
 $token = $youCanPay->token->create("order-id", "2000", "USD", "123.123.123.123");
