4e673be94eb6518ee71ce83cac4f2d85b8fcca46
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-02 21:43:14 +0100

    reading user's numbers

--- Controllers/payController.php
+++ Controllers/payController.php
@@ -1,14 +1,11 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-//use YouCan\Pay\YouCanPay;
 require '../vendor/autoload.php';
 
 error_reporting(E_ERROR);
 $youCanPay = \YouCan\Pay\YouCanPay::instance()->useKeys('pri_sandbox_9858abcb-be2c-4e3c-bbc6-8c7af', 'pub_sandbox_4a6c718a-4a4e-4d3b-afbd-9d30e');
 
-$transaction = $youCanPay->transaction->get("transaction-id");
+$token = $youCanPay->token->create("order-id", "2000", "USD", "123.123.123.123");
 
-if (is_null($transaction)) {
-    echo "TRANSACTION NULL";
-}
+echo $token->getId();
