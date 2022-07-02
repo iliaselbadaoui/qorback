c6b8f3cbc6b3ba2a02acba5c3ada7a2c5f9d8e0b
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-02 01:16:40 +0100

    reading user's numbers

--- Controllers/payController.php
+++ Controllers/payController.php
@@ -2,6 +2,8 @@
 namespace YouCan\Pay;
 use YouCan\Pay\YouCanPay as YouCanPay;
 
+error_reporting(E_ERROR);
+
 $youCanPay = YouCanPay::instance()->useKeys('pri_key', 'pub_key');
 
 $transaction = $youCanPay->transaction->get("transaction-id");
