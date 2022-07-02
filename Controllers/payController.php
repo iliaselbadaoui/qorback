a9d987141314ba942e1e1729d5ee7eb7c6d0f6d5
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-02 22:22:18 +0100

    reading user's numbers

--- Controllers/payController.php
+++ Controllers/payController.php
@@ -6,6 +6,6 @@ require '../vendor/autoload.php';
 error_reporting(E_ERROR);
 $youCanPay = \YouCan\Pay\YouCanPay::instance()->useKeys('pri_sandbox_9858abcb-be2c-4e3c-bbc6-8c7af', 'pub_sandbox_4a6c718a-4a4e-4d3b-afbd-9d30e');
 
-$token = $youCanPay->token->create("070220221", "2000", "MAD", "123.123.123.123");
+$token = $youCanPay->token->create("070220221", "2000", "MAD", "172.17.0.1");
 
 echo $token->getId();
