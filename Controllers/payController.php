dbd0c63f206f569eb55263237dca9fd6f6781bad
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-02 01:02:40 +0100

    reading user's numbers

--- Controllers/payController.php
+++ Controllers/payController.php
@@ -1,2 +1,11 @@
 <?php
+//namespace YouCan\Pay;
+use YouCan\Pay\YouCanPay;
 
+$youCanPay = YouCanPay::instance()->useKeys('pri_key', 'pub_key');
+
+$transaction = $youCanPay->transaction->get("transaction-id");
+
+if (is_null($transaction)) {
+    echo "TRANSACTION NULL";
+}
