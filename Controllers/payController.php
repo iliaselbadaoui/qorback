a49ddad20b4559c155ce8389597c374ab14c1deb
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-02 17:37:26 +0100

    reading user's numbers

--- Controllers/payController.php
+++ Controllers/payController.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace YouCan\Pay;
 use YouCan\Pay\YouCanPay as YouCanPay;
+header('Access-Control-Allow-Origin: *');
 
 error_reporting(E_ERROR);
-
 $youCanPay = YouCanPay::instance()->useKeys('pri_key', 'pub_key');
 
 $transaction = $youCanPay->transaction->get("transaction-id");
