dbc7a39f0811691e6b5acd44c09287877454485c
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-02 18:21:35 +0100

    reading user's numbers

--- Controllers/payController.php
+++ Controllers/payController.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Origin: *');
 require '../vendor/autoload.php';
 
 error_reporting(E_ERROR);
-$youCanPay = new \YouCan\Pay\YouCanPay(new \YouCan\Pay\API\HTTPAdapter\HTTPAdapterPicker());
+$youCanPay = \YouCan\Pay\YouCanPay::instance()->useKeys('pri_sandbox_9858abcb-be2c-4e3c-bbc6-8c7af', 'pub_sandbox_4a6c718a-4a4e-4d3b-afbd-9d30e');
 
 $transaction = $youCanPay->transaction->get("transaction-id");
 
