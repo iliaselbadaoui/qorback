c24bb931124d20ecd9b5a1be495251084ee8bd03
Ilias Elbadaoui <iliassovich@outlook.com>
2022-07-07 20:42:35 +0100

    likepay integration

--- Services/listingServices.php
+++ Services/listingServices.php
@@ -60,7 +60,7 @@ class listingServices
 
     public function getUserTickets($id)
     {
-        $prepared = $this->connect->prepare("CALL `get_user_participations(?)");
+        $prepared = $this->connect->prepare("CALL get_user_participations(?)");
         $prepared->execute($id);
         $rawData = $prepared->fetchAll(PDO::FETCH_ASSOC);
         $newData = array();
